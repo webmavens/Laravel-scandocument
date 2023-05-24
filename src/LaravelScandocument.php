0cffc22700ce2777f9f016f8f9316493114ec0e0
sonali <sonali@webmavens.com>
2023-05-24 14:54:19 +0530

    New function for image add

--- src/LaravelScandocument.php
+++ src/LaravelScandocument.php
@@ -7,17 +7,21 @@ use Webmavens\LaravelScandocument\Services\LaravelScandocumentService;
 
 class LaravelScandocument
 {
-	public static function sendDocToScan($imgPath,$jobTag = null)
+	public static function sendDocToScan($imgPath,$jobTag = null, $type = null)
 	{
         $jobId = '';
-        $result = LaravelScandocumentService::sendDoc($imgPath,$jobTag);
-        $laravelScandocument = new LaravelScandocumentData;
-        $laravelScandocument->path = $imgPath;
-        if ($result) {
-        	$laravelScandocument->jobid = $result['JobId'];
-            $jobId = $result['JobId'];
+        if ($type == 'Image') {
+            $result = LaravelScandocumentService::sendDocForImage($imgPath,$jobTag);
+        } else {
+            $result = LaravelScandocumentService::sendDoc($imgPath,$jobTag);
+            $laravelScandocument = new LaravelScandocumentData;
+            $laravelScandocument->path = $imgPath;
+            if ($result) {
+                $laravelScandocument->jobid = $result['JobId'];
+                $jobId = $result['JobId'];
+            }
+            $laravelScandocument->save();
+            return $jobId;
         }
-        $laravelScandocument->save();
-        return $jobId;
 	}
 }
